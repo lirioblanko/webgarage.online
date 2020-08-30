e7e11b4b82398b2f477fda26a41dd6fe177465d0
lirioblanko <goldliliya@mail.ru>
2020-08-30 10:44:03 +0300

    scroll-test

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,22 @@
 import '../styles/index.scss'
 // import '../styles/index.sass'
 // import '../styles/index.less'
+
+
+
+let el = document.querySelector('.toggleClass');
+
+
+window.addEventListener('scroll', toggleClassOnScroll.bind(el, 1));
+
+function toggleClassOnScroll(pxAmount) {
+    let scrollTop = document.body.scrollTop;
+
+    if(scrollTop < pxAmount) {
+        this.classList.add('toggleClass--active');
+        console.log(el)
+    } else {
+        this.classList.remove('toggleClass--active');
+
+    }
+}
