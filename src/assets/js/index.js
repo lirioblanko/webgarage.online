1f49d0dc85927b24f47f818cedb96939db7a83ff
lirioblanko <goldliliya@mail.ru>
2020-08-31 15:55:21 +0300

    fixChanges

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,20 +3,15 @@ import '../styles/index.scss'
 // import '../styles/index.less'
 
 
-
 let el = document.querySelector('.toggleClass');
+document.addEventListener('scroll', toggleClassOnScroll.bind(el, 1));
 
-
-window.addEventListener('scroll', toggleClassOnScroll.bind(el, 1));
-
-function toggleClassOnScroll(pxAmount) {
-    let scrollTop = document.body.scrollTop;
-
-    if(scrollTop < pxAmount) {
+function toggleClassOnScroll(pxAmount, e) {
+    let scrollTop = window.scrollY;
+console.log(scrollTop)
+    if(scrollTop > 0) {
         this.classList.add('toggleClass--active');
-        console.log(el)
     } else {
         this.classList.remove('toggleClass--active');
-
     }
 }
