de0e92594d959bd6a8c6e7344c77026c0916307e
lirioblanko <goldliliya@mail.ru>
2020-09-02 13:11:30 +0300

    fixChanges

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -15,3 +15,12 @@ console.log(scrollTop)
         this.classList.remove('toggleClass--active');
     }
 }
+
+import decorSrc from '../img/decor_el.svg'
+const decor = document.querySelector('.decor')
+for (let i=0; i<30; i++) {
+    const img = document.createElement('img');
+    img.setAttribute('src', decorSrc);
+    img.classList.add('decor-item');
+    decor.appendChild(img);
+}
