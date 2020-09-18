5f7bcbd4331a4b85c855b2e287afd6360f46ebd3
Vitaliy Shishorin <moskrc@gmail.com>
2020-09-18 13:23:07 +0300

    feedback form

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,82 +1,85 @@
-import '../styles/index.scss'
-// import '../styles/index.sass'
-// import '../styles/index.less'
+import "../styles/index.scss";
 
-
-let el = document.querySelector('.toggleClass');
-document.addEventListener('scroll', toggleClassOnScroll.bind(el, 1));
+let el = document.querySelector(".toggleClass");
+document.addEventListener("scroll", toggleClassOnScroll.bind(el, 1));
 
 function toggleClassOnScroll(pxAmount, e) {
-    let scrollTop = window.scrollY;
-    if(scrollTop > 0) {
-        this.classList.add('toggleClass--active');
-    } else {
-        this.classList.remove('toggleClass--active');
-    }
+  let scrollTop = window.scrollY;
+  if (scrollTop > 0) {
+    this.classList.add("toggleClass--active");
+  } else {
+    this.classList.remove("toggleClass--active");
+  }
 }
 
-import decorSrc from '../img/decor_el.svg'
-const decor = document.querySelector('.decor')
-for (let i=0; i<30; i++) {
-    const img = document.createElement('img');
-    img.setAttribute('src', decorSrc);
-    img.classList.add('decor-item');
-    decor.appendChild(img);
+import decorSrc from "../img/decor_el.svg";
+const decor = document.querySelector(".decor");
+for (let i = 0; i < 30; i++) {
+  const img = document.createElement("img");
+  img.setAttribute("src", decorSrc);
+  img.classList.add("decor-item");
+  decor.appendChild(img);
 }
+
+// feedback form
+
+const feedback_form = document.querySelector(".form-feedback");
+const success_msg = document.querySelector(".success-msg");
 
 function error(input, message) {
-    input.className = 'error';
-    // show the error message
-    const error = input.previousElementSibling;
-    error.innerText = message;
-    return false;
+  input.className = "form__item error";
+  // show the error message
+  const error = input.nextElementSibling;
+  error.innerText = message;
+  return false;
 }
 
 function success(input) {
-    input.className = 'success';
-    // hide the error message
-    const error = input.previousElementSibling;
-    error.innerText = '';
-    return true;
-}
-
-function requireValue(input, message) {
-    return input.value.trim() === '' ?
-        error(input, message) :
-        success(input);
+  input.className = "form__item success";
+  // hide the error message
+  const error = input.nextElementSibling;
+  error.innerText = "";
+  return true;
 }
 
 function validateEmail(input) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    return re.test(input.value.trim()) ?
-        success(input) :
-        error(input, 'Invalid email format');
+  return re.test(input.value.trim())
+    ? success(input)
+    : error(input, "Неверный адрес электронной почты");
 }
 
-async function sendEmail(subject, message) {
-    const url = 'https://x0usjw1m3c.execute-api.eu-west-2.amazonaws.com/Prod/send';
-    const toEmails = ['moskrc@gmail.com', 'sprashivaesh@yandex.ru', 'goldliliya@mail.ru']
-    const data = {"subject": subject, "message": message, "toEmails": toEmails};
-    
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+async function sendEmail(sbj, msg) {
+  const url =
+    "https://x0usjw1m3c.execute-api.eu-west-2.amazonaws.com/Prod/send";
+  const toEmails = [
+    "moskrc@gmail.com",
+    "sprashivaesh@yandex.ru",
+    "goldliliya@mail.ru",
+  ];
+  const data = { subject: sbj, message: msg, toEmails: toEmails };
 
-      // TODO: add 'Thank you!' message
-    } catch (error) {
-        console.log(error)
-        alert('ошибка')
-    }
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+
+    feedback_form.reset();
+    feedback_form.style.display = "none";
+    success_msg.style.display = "block";
+    success_msg.style.opacity = 1;
+  
+    return response
+  } catch (err) {
+    console.log(err);
+  }
 }
-
-const feedback_form = document.querySelector('.form-feedback');
-
 
 //  this is normalno
 const name = feedback_form.elements[0];
@@ -84,34 +87,11 @@ const email = feedback_form.elements[1];
 const phone = feedback_form.elements[2];
 const message = feedback_form.elements[3];
 
-
-
-const requiredFields = [
-    {input: name, message: 'Name is required'},
-    {input: email,message: 'Email is required'}
-];
-
-feedback_form.addEventListener('submit', (event) => {
-    event.preventDefault();
+feedback_form.addEventListener("submit", (event) => {
+  event.preventDefault();
   
-    // check required fields
-    let valid = true;
-
-    requiredFields.forEach((input) => {
-        valid = requireValue(input.input, input.message);
-    });
-
-    // validate email
-    if (valid) {
-        valid = validateEmail(email);
-    }
-
-    // stop submitting the form if the data is invalid
-    if (valid) {
-        const message_to_send = `Сделайте мне уже что нибудь. Меня зовут: ${name.value}. Мой телефон: ${phone.value}. А электронная почта: ${email.value}. Вот мой сообщение вам: ${message.value}`;
-        sendEmail('Заказ с сайта', message_to_send )
-    } else {
-        alert('Fff')
-    }    
+  if (validateEmail(email)) {
+    const message_to_send = `Имя: ${name.value}\nТелефон: ${phone.value}\nПочта: ${email.value}\nСообщение: \n${message.value}\n`;
+    sendEmail("Заказ с сайта", message_to_send);
+  }
 });
-
